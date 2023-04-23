0d7489acafca97e625c6b87168afa235d8aeccab
gustavo-munhoz <gustavo.munhoz015@gmail.com>
2023-04-23 20:24:22 -0300

    Included posts route

--- api.js
+++ api.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const session = require('express-session')
-const axios = require('axios')
-const dotenv = require('dotenv')
+const express = require('express');
+const session = require('express-session');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const path = require('path');
 
 // configure environment variable access
-dotenv.config()
+dotenv.config();
 
 // configure express and port
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // configure session
 app.use(session({
@@ -17,6 +18,15 @@ app.use(session({
     saveUninitialized: false,
     cookie: { secure: false }
 }));
+
+// default route leading to front-end
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'styles.css'));
+});
 
 // route to access to redirect to facebook login page
 app.get('/auth/facebook', (req, res) => {
@@ -27,7 +37,7 @@ app.get('/auth/facebook', (req, res) => {
         `client_id=${process.env.FACEBOOK_APP_ID}`,
         `redirect_uri=http://localhost:${port}/auth/facebook/callback`,
         'response_type=code',
-        'scope=email,public_profile',
+        'scope=email,public_profile,user_posts',
         `state=${state}`
     ]
 
@@ -47,12 +57,15 @@ app.get('/auth/facebook/callback', async (req, res) => {
 
     try {
         const response = await axios.get(`${accessTokenUrl}?${queryParams.join('&')}`);
-        const accessToken = response.data.access_token;
-        req.session.access_token = accessToken;
-        res.send(`Obtained access token: ${accessToken}.`);
+        req.session.access_token = response.data.access_token;
+        res.redirect('/menu');
     } catch (error) {
         res.status(500).send('Error while obtaining access token.');
     }
+});
+
+app.get('/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'menu.html'));
 });
 
 // route to get profile information: name, email and picture sent as JSON in response
@@ -69,6 +82,20 @@ app.get('/profile', async (req, res) => {
     } else {
         res.status(401).send('User is not authenticated.');
     }
+});
+
+
+app.get('/posts', async (req, res) => {
+   if (req.session && req.session.access_token) {
+       try {
+           const response = await axios.get(
+               `https://graph.facebook.com/v16.0/me/posts?access_token=${req.session.access_token}`);
+           const posts = response.data;
+           res.json(posts);
+       } catch (error) {
+           res.status(500).send('Error retrieving posts');
+       }
+   } else res.status(401).send('User is not authenticated');
 });
 
 app.listen(port, () => {
